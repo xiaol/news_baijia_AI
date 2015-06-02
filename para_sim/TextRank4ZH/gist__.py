0df05561cb876003c2d87efd37c57341a26289af
Gavin <Gavin@WeiliangGuos-Mac.local>
2015-06-02 11:18:07 +0800

    get_gist() finished;cal_sim() needs to be done

--- para_sim/TextRank4ZH/gist__.py
+++ para_sim/TextRank4ZH/gist__.py
@@ -29,36 +29,30 @@ class Gist:
     # tr4s.train(text=text, speech_tag_filter=True, lower=True, source = 'all_filters')
     # gresult = ' '.join(tr4s.get_key_sentences(num=3)) # 重要性最高的三个句子
 
-    def __init__(self, stop_words_file='/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/stopword.data'):
-            self.stop_words_file=stop_words_file
-
-    def get_keyword(self, text):
-            self.tr4w = TextRank4Keyword(self.stop_words_file)  # 导入停止词
+    def __init__(self, text = codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/01.txt', 'r', 'utf-8').read()):
+            stop_words_file='/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/stopword.data'
+            tr4w = TextRank4Keyword(stop_words_file)  # 导入停止词
             #使用词性过滤，文本小写，窗口为2
-            self.tr4w.train(text=text, speech_tag_filter=True, lower=True, window=2)
+            tr4w.train(text=text, speech_tag_filter=True, lower=True, window=2)
             # 20个关键词且每个的长度最小为1
-            self.wresult = ' '.join(self.tr4w.get_keywords(20, word_min_len=1))
-            return self.wresult
+            self.wresult = ' '.join(tr4w.get_keywords(20, word_min_len=1))
+            # 20个关键词去构造短语，短语在原文本中出现次数最少为2
+            self.presult = ' '.join(tr4w.get_keyphrases(keywords_num=20, min_occur_num= 2))
+            tr4s = TextRank4Sentence(stop_words_file)
+            # 使用词性过滤，文本小写，使用words_all_filters生成句子之间的相似性
+            tr4s.train(text=text, speech_tag_filter=True, lower=True, source = 'all_filters')
+            self.gresult = ' '.join(tr4s.get_key_sentences(num=3)) # 重要性最高的三个句子
+
+    def get_keyword(self):
+        return self.wresult
 
     def get_keyphrase(self):
-            # 20个关键词去构造短语，短语在原文本中出现次数最少为2
-            self.presult = ' '.join(self.tr4w.get_keyphrases(keywords_num=20, min_occur_num= 2))
-            self.tr4s = TextRank4Sentence(self.stop_words_file)
-            return self.presult
+        return self.presult
 
-    def get_gist(self, text):
-            self.tr4w = TextRank4Keyword(self.stop_words_file)  # 导入停止词
-            #使用词性过滤，文本小写，窗口为2
-            self.tr4w.train(text=text, speech_tag_filter=True, lower=True, window=2)
-            # 20个关键词且每个的长度最小为1
-            self.wresult = ' '.join(self.tr4w.get_keywords(20, word_min_len=1))
-            # 20个关键词去构造短语，短语在原文本中出现次数最少为2
-            self.presult = ' '.join(self.tr4w.get_keyphrases(keywords_num=20, min_occur_num= 2))
-            self.tr4s = TextRank4Sentence(self.stop_words_file)
-            # 使用词性过滤，文本小写，使用words_all_filters生成句子之间的相似性
-            self.tr4s.train(text=text, speech_tag_filter=True, lower=True, source = 'all_filters')
-            self.gresult = ' '.join(self.tr4s.get_key_sentences(num=3)) # 重要性最高的三个句子
-            return self.gresult
+    def get_gist(self):
+        return self.gresult
+
+
 
 
 def cal_sim(textList):
@@ -77,34 +71,24 @@ def cal_sim(textList):
     index = similarities.MatrixSimilarity(lsi[corpus]) # transform corpus to LSI space and index it
     index.save('/Users/Gavin/work/news_baijia_AI/para_sim/gist.index')
     index = similarities.MatrixSimilarity.load('/Users/Gavin/work/news_baijia_AI/para_sim/gist.index')
-    for x in range(len(textList)):
-        vec_lsi = lsi[corpus[x]]
-        sims = index[vec_lsi]
-        sims_list =[]
-        sims_list.append(list(enumerate(sims)))
-    return sims_list
-
-    # vec_lsi = lsi[corpus[0]]
-    # sims = index[vec_lsi]
-
-
-
+    vec_lsi = lsi[corpus[0]]
+    sims = index[vec_lsi]
     # sims = sorted(enumerate(index), key=lambda item: item[1])
     # print list(enumerate(index))
     # print(list(enumerate(index)))
     # return lsi.print_topics(2)
     # for d print simsoc in corpus_lsi:
-    # return list(enumerate(sims))
+    return list(enumerate(sims))
 
 
-# def get_one_doc_gist(content):
-#     gist = ''
-#     return gist
+def get_one_doc_gist(content):
+    gist = ''
+    return gist
 
 if __name__ == "__main__":
-    a = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/01.txt', 'r', 'utf-8').read())
-    b = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/02.txt', 'r', 'utf-8').read())
-    c = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/03.txt', 'r', 'utf-8').read())
+    a = Gist().get_gist()
+    b = Gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/02.txt', 'r', 'utf-8').read()).get_gist()
+    c = Gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/03.txt', 'r', 'utf-8').read()).get_gist()
 
     ag = list(jieba.cut(a))
     bg = list(jieba.cut(b))
@@ -113,9 +97,9 @@ if __name__ == "__main__":
     textList.append(ag)
     textList.append(bg)
     textList.append(cg)
-    # print cal_sim(textList)
-    print a
+
     print cal_sim(textList)
+
     # get_one_doc_gist("中国人是这样的")
 
 
