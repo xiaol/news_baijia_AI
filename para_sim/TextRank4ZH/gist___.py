123a3edb9ae263971e10eee76a7e4b78e29c9c5d
Gavin <Gavin@WeiliangGuos-Mac.local>
2015-06-04 16:51:40 +0800

    gist method for one sentence implemented, sim cal simplified

--- para_sim/TextRank4ZH/gist___.py
+++ para_sim/TextRank4ZH/gist___.py
@@ -46,37 +46,62 @@ class Gist:
             self.gresult = ' '.join(self.tr4s.get_key_sentences(num=1)) # 重要性最高的三个句子
             return self.gresult
 
-#query is a string, textList is a list of strings.
-def cal_sim(query, textList):
+from pprint import pprint
+def cal_sim(textList):
 
     textList = [list(jieba.cut(text)) for text in textList]
-    print jieba.cut(query)
     dictionary = corpora.Dictionary(textList)
     corpus = [dictionary.doc2bow(text) for text in textList]
     corpora.MmCorpus.serialize('gist.mm', corpus)
     corpus = corpora.MmCorpus('gist.mm')
     print corpus
+    tfidf = models.TfidfModel(corpus) # step 1 -- initialize a model
+    # corpus_tfidf = tfidf[corpus]
+
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2) # initialize an LSI transformation
-    query_bow = dictionary.doc2bow(jieba.cut(query))
-    query_lsi = lsi[query_bow]
     index = similarities.MatrixSimilarity(lsi[corpus]) # transform corpus to LSI space and index it
-    sims = index[query_lsi]
-    print sims
-    return sims
+    sims_list =[]
+    for x in range(len(textList)):
+        corpus[x] = dictionary.doc2bow(textList[x])
+        vec_lsi = lsi[corpus[x]]
+        sims = index[vec_lsi]
+        sims_list.append(list(enumerate(sims)))
+    r = {}
+    w = []
+    for sl in sims_list:
+        for v,k in sl:
+            r[v] = k
+        w.append(r)
+    dl = [dict(t) for t in sims_list]
+    # for m in dl:
+    #     if min()
+    mdl = []
+
+
+    for x in dl:
+        min_v = min(x.itervalues())
+        mdlo = {k:v for k,v in x.iteritems() if v == min_v}
+        # mdl.append(min_v)
+        mdl.append(mdlo)
+    smdl = sorted(mdl, key = lambda k:k)
+    # fr = sorted(dl)
+
+    print smdl
+    print sims_list
 
 if __name__ == "__main__":
     a = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/01.txt', 'r', 'utf-8').read())
     b = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/02.txt', 'r', 'utf-8').read())
     c = Gist().get_gist(codecs.open('/Users/Gavin/work/news_baijia_AI/para_sim/TextRank4ZH/text/05.txt', 'r', 'utf-8').read())
 
-    x = '上个周末，吉林农业科技学院经济管理学院赵同学，本来是因为得到了一张长春市的演出邀请券，从吉林市到长春市玩，不想在重庆路逛街的途中竟看到了一个正在行窃的小偷，并用手机拍下了小偷行窃的全过程。 '
-    y = '这小偷还挺有职业道德，只偷钱，又把钱包放回去了，钱包里都是各种卡啊身份证啥的，补办起来很麻烦，很体贴的小偷，赞一个。'
+    x = '上个周末，吉林农业科技学院经济管理学院赵同学。'
+    y = '这小偷还挺有职业道德，只偷钱，又把钱包放回去了。'
 
     textList = []
     textList.append(x)
     textList.append(y)
     # textList.append(c)
-    cal_sim(x, textList)
+    cal_sim(textList)
 
 
 
